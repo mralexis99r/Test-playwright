e24169545bfe42dece388c072e9fb8d866a474f3
mralexis99r <alexissantiagobusiness@gmail.com>
2025-06-09 17:14:13 -0600

    Add folder to evidence and get better management

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -6,14 +6,14 @@ test.describe("Add to Cart", () => {
     test('add USB hub to cart', async({page}) => {
         await test.step("User go to URL Amazon", async () => {
             await page.goto('https://www.amazon.com.mx/');
-            await page.screenshot({path: "./Evidence/Captures" + Date.now() + "screenshot.png"});
+            await page.screenshot({path: "./Evidence/add-to-cart/Captures" + Date.now() + "screenshot.png"});
         });
 
         await test.step("User search 'USB Hub' in the input browser", async({}) => {
             await expect(page.locator('#twotabsearchtextbox')).toBeVisible();
             await page.fill('#twotabsearchtextbox', 'USB Hub');
             await page.keyboard.press('Enter');
-            await page.screenshot({path: "./Evidence/Captures" + Date.now() + "screenshot.png"});
+            await page.screenshot({path: "./Evidence/add-to-cart/Captures" + Date.now() + "screenshot.png"});
             await page.pause();
             
         });
