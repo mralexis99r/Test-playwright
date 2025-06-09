13e31e2cf1bb0a235b98194aa8dcfdd07b185890
mralexis99r <alexissantiagobusiness@gmail.com>
2025-06-09 16:16:24 -0600

    Screenshot is added in the code to evidence

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -6,6 +6,8 @@ test.describe("Add to Cart", () => {
     test('add USB hub to cart', async({page}) => {
         await test.step("User go to URL Amazon", async () => {
             await page.goto('https://www.amazon.com.mx/');
+            await page.screenshot({path: "./Evidence/Captures" + Date.now() + "screenshot.png"});
+
         });
 
 
