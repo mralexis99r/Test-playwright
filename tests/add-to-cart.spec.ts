42f0fdee583bd1d2c037231dfa2b52b42476401c
mralexis99r <alexissantiagobusiness@gmail.com>
2025-06-10 01:31:53 -0600

    modify code with json data to test

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -2,59 +2,28 @@ import { test, expect } from '@playwright/test';
 import { describe } from 'node:test';
 import products from '../data/products.json'; // <-- here brings data
 
-test.describe("Add to Cart", () => {
+test.describe('Add to Cart', () => {
+  for (const product of products) {
+    test(`Add ${product.name} to cart`, async ({ page }) => {
+      // STEP 1: GO TO AMAZON
+      await page.goto('https://www.amazon.com.mx/');
 
-    test('add USB hub to cart', async({page}) => {
-        await test.step("User go to URL Amazon", async () => {
-            await page.goto('https://www.amazon.com.mx/');
-            await page.screenshot({path: "./Evidence/add-to-cart/Captures" + Date.now() + "screenshot.png"});
-        });
+      // STEP 2: SEARCH PRODUCT
+      await page.locator('#twotabsearchtextbox').fill(product.name);
+      await page.keyboard.press('Enter');
+      await page.waitForTimeout(2000);
 
-        await test.step("User search 'USB Hub' in the input browser", async({}) => {
-            await expect(page.locator('#twotabsearchtextbox')).toBeVisible();
-            await page.fill('#twotabsearchtextbox', 'USB Hub');
-            await page.keyboard.press('Enter');
-            await page.waitForTimeout(2000) //added timeout for await for the screenshot
-            await page.screenshot({path: "./Evidence/add-to-cart/" + Date.now() + "screenshot.png"});
-            
-        });
+      // STEP 3: CLICK TO RESULT
+      await page.locator('.s-main-slot .s-result-item').first().click();
+      await page.waitForTimeout(2000);
 
-        await test.step("User click on result of the search", async ({}) => {
+      // STEP 4: ADD TO CART
+      await page.click('#add-to-cart-button');
+      await page.waitForTimeout(2000);
 
-            await page.locator('.s-main-slot .s-result-item').first().waitFor({ state: 'visible' });
-            await page.locator('.s-main-slot .s-result-item').nth(0).click();
-            await page.waitForTimeout(2000) //Added timeout for the screenshot
-            await page.screenshot({ path: "./Evidence/add-to-cart/" + Date.now() + "screenshot.png" });
-        
-        });
-
-        await test.step("User click on 'Add to Cart'", async ({}) => {
-
-            await page.click("#add-to-cart-button");
-            await page.waitForTimeout(2000) //Added timeout for the screenshot
-            await page.screenshot({ path: "./Evidence/add-to-cart/" + Date.now() + "screenshot.png" });
-            
-        
-        });
-
-        await test.step("User click on 'proceedToRetailCheckout'", async ({}) => {
-
-            await page.getByRole('button', {name: 'Proceder al pago'}).click();
-            await page.waitForTimeout(2000) //added timeout for the screenshot
-            await page.screenshot({ path: "./Evidence/add-to-cart/" + Date.now() + "screenshot.png" });
-            
-        
-        });
-
-
-        
-        
-
-       
-        
-        
-
-
+      // STEP 5: PROCEED TO CHECKOUT
+      await page.getByRole('button', { name: /proceder al pago/i }).click();
+      await page.waitForTimeout(2000);
     });
-
+  }
 });
